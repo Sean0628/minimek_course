1a582c6877c1f3ffea67b4d44137acd2766509f2
Sho Ito <i.sho.0628@gmail.com>
2019-02-16 15:58:52 +0900

    enable absolute import paths

--- src/features/tabs/TabBar.jsx
+++ src/features/tabs/TabBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Menu} from "semantic-ui-react";
-import ToggleDisplay from "../../common/components/ToggleDisplay"
+import ToggleDisplay from "common/components/ToggleDisplay"
 
 import Tab from "./Tab";
 
