ab32ddc321784d48cf9cf5669d41187434c338cd
Sho Ito <i.sho.0628@gmail.com>
2019-02-17 18:23:58 +0900

    connect the mechs component to display a list of mechs from the store

--- src/features/mechs/MechsList/MechsListRow.jsx
+++ src/features/mechs/MechsList/MechsListRow.jsx
@@ -6,10 +6,15 @@ import {getWeightClass} from "../mechSelectors";
 const MechsListRow = ({mech={}}) => {
   const {
     id = "",
-    name = "",
     type = "",
-    weight = "",
+    mechType = {},
   } = mech;
+
+  const {
+    name = "",
+    weight = "",
+  } = mechType;
+
   const weightClass = getWeightClass(weight);
 
   return (
