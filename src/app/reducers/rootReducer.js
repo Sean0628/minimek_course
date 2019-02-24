b0ef542b2082ce76f5333974eaa74f254ad2d7fa
Sho Ito <i.sho.0628@gmail.com>
2019-02-24 15:23:58 +0900

    update root reducer to wrap around combinded reducer

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,12 +1,14 @@
 import {combineReducers} from "redux";
 
+import {reduceReducers} from "common/utils/reducerUtils";
+
 import entitiesReducer from "./entitiesReducer";
 import pilotsReducer from "features/pilots/pilotsReducer";
 import mechsReducer from "features/mechs/mechsReducer";
 import tabReducer from "features/tabs/tabsReducer";
 import unitInfoReducer from "features/unitInfo/unitInfoReducer";
 
-const rootReducer = combineReducers({
+const combinedReducer = combineReducers({
   entities : entitiesReducer,
   pilots : pilotsReducer,
   mechs : mechsReducer,
@@ -14,5 +16,9 @@ const rootReducer = combineReducers({
   tabs : tabReducer,
 });
 
+
+const rootReducer = reduceReducers(
+  combinedReducer,
+);
 
 export default rootReducer;
