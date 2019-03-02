481be402a9aa5cf18a3587e28dfd92098386b806
Sho Ito <i.sho.0628@gmail.com>
2019-03-02 22:59:44 +0900

    add logic to reset a currently edited item

--- src/features/editing/editingActions.js
+++ src/features/editing/editingActions.js
@@ -3,6 +3,7 @@ import {
   EDIT_ITEM_UPDATE,
   EDIT_ITEM_APPLY,
   EDIT_ITEM_STOP,
+  EDIT_ITEM_RESET,
 } from "./editingConstants";
 
 export function editExistingItem(itemType, itemID) {
@@ -39,6 +40,16 @@ export function editItemAttributes(itemType, itemID, newItemAttributes) {
 export function stopEditingItem(itemType, itemID) {
   return {
     type : EDIT_ITEM_STOP,
+    payload : {
+      itemType,
+      itemID,
+    },
+  };
+}
+
+export function resetEditedItem(itemType, itemID) {
+  return {
+    type : EDIT_ITEM_RESET,
     payload : {
       itemType,
       itemID,
